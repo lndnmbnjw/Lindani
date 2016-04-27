1f15d2b574f742f5e794f506b4d1e53f976d56b3
lndnmbnjw <lndnmbnjw@gmail.com>
2016-04-28 00:05:31 +0200

    Added files via upload

---
+++ Tutorial 2 - Question 3 [April 2016].py
@@ -0,0 +1,17 @@
+Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> # April 2016 - Tutorial 2: Question 3
+>>> # Question:(i) How can I take all odd points from an array?
+>>> 
+>>> 
+>>> # Take b=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+>>> 
+>>> b=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+>>> b[1::2]
+[1, 3, 5, 7, 9, 11, 13, 15]
+>>> 
+>>> # Question:(ii) How can I take all even points from an array?
+>>> b[0::2]
+[0, 2, 4, 6, 8, 10, 12, 14]
+>>> 
